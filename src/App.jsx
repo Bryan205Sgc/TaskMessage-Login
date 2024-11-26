e8dbf70104500776b7c745120f879fe244525804
bryan205 <bryangomez19032004@gmail.com>
2024-11-25 19:42:56 -0600

    regreso a commit funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import TaskBoard from './components/TaskBoard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-
-
+import Login from './components/Login';
+import TaskBoard from './components/TaskBoard';
 
 const App = () => {
   return (
     <DndProvider backend={HTML5Backend}>
-      <TaskBoard />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<TaskBoard />} />
+        </Routes>
+      </Router>
     </DndProvider>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-
-import Root from './routes/Routs';
-
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+  <DndProvider backend={HTML5Backend}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </DndProvider>
 );
